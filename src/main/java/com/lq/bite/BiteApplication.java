ef91fe2bbf9e1d9917beda5a4f088c595bf16541
827244716@qq.com <827244716@qq.com>
2018-01-10 13:41:45 +0800

    初始化

---
+++ src/main/java/com/lq/bite/BiteApplication.java
@@ -0,0 +1,12 @@
+package com.lq.bite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteApplication.class, args);
+	}
+}
